dc684d7638a9e6eab9518cba989f91a292cff8a2
Md. Janay Alam <janayalam.work@gmail.com>
2023-12-15 13:03:57 +0600

    feat: design the navigation bar (Refs#1)
    Added dark and light theme and a toggle button in the navigation bar.
    
    next: Design the navigation bar properly. Add multiple language feature.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,25 @@
+<script setup>
+import { onMounted } from 'vue';
+import Navbar from './components/ui/Navbar.vue';
+import { useTheme } from 'vuetify';
+
+const theme = useTheme();
+
+onMounted(() => {
+  // setting theme from local storage
+  const localTheme = localStorage.getItem('theme') || 'light';
+  localTheme == 'dark'
+    ? theme.global.name.value = 'dark'
+    : theme.global.name.value = 'light';
+
+});
+</script>
+
 <template>
-  <div>Weather App</div>
+  <v-app>
+    <navbar />
+  </v-app>
 </template>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -4,30 +4,23 @@ import './styles/reset.css';
 import Toast from 'vue-toastification';
 import 'vue-toastification/dist/index.css';
 
-import App from './App.vue';
-
-const app = createApp(App);
-
 // vuetify
-import 'vuetify/styles';
-import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
+import vuetify from './vuetify';
 
-const vuetify = createVuetify({
-  components,
-  directives,
-});
-
-// vuetify
+// fontawesome
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faUserSecret);
+library.add(fas);
 
 // importing custom css
 import './styles/main.css';
+
+// importing app component
+import App from './App.vue';
+
+const app = createApp(App);
 
 app.use(vuetify);
 app.use(Toast);
